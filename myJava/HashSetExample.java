def7ec1461c87a6eef2e6eba622b357252e97935
gjason <46449309+Jason017@users.noreply.github.com>
2020-12-20 06:22:03 -0500

    Update HashSetExample.java

--- myJava/HashSetExample.java
+++ myJava/HashSetExample.java
@@ -14,19 +14,19 @@ public class HashSetExample {
         // To find union
         Set<Integer> union = new HashSet<Integer>(a);
         union.addAll(b);
-        System.out.print("Union of the two Set");
+        System.out.print("Union of the two Set: ");
         System.out.println(union);
 
         // To find intersection
         Set<Integer> intersection = new HashSet<Integer>(a);
         intersection.retainAll(b);
-        System.out.print("Intersection of the two Set");
+        System.out.print("Intersection of the two Set: ");
         System.out.println(intersection);
 
         // To find the symmetric difference
         Set<Integer> difference = new HashSet<Integer>(a);
         difference.removeAll(b);
-        System.out.print("Difference of the two Set");
+        System.out.print("Difference of the two Set: ");
         System.out.println(difference);
     }
 }
